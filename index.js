0a5140a146a40550d5a54029e025e9326f91238d
miguel-124 <mikicuellar20@gmail.com>
2025-09-19 02:05:18 -0400

    Add color picker and reset functionality to clock interface

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const rootElement = document.documentElement;
 
 const clock = document.querySelector(".clock");
 const dateInfo = document.querySelector(".date-info");
+const infoText = document.querySelector(".info-text");
+const changeColorBtn = document.querySelector(".change-color");
+const inputColor = document.getElementById("input-color");
+const resetDefaultColors = document.querySelector(".reset-default-colors");
 const time = document.querySelector(".time");
 
 const currentDate = new Date();
@@ -16,7 +20,7 @@ const months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto
 const days = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'];
 
 function mostrarDataDate(){
-    dateInfo.innerHTML += `${days[currentDate.getDay()]} ${currentDate.getDate()} de ${months[currentDate.getMonth()]} del ${currentDate.getFullYear()}`;
+    infoText.textContent += `${days[currentDate.getDay()]} ${currentDate.getDate()} de ${months[currentDate.getMonth()]} del ${currentDate.getFullYear()}`;
     time.textContent = getTimeFormat( currentDate );
 }
 
@@ -65,19 +69,6 @@ const changeColors = setInterval(() => {
 
 const stopChangeColors = ()=> clearInterval(changeColors);
 
-function setColorFijo() {
-    stopChangeColors();
-    const inputColor = document.createElement("input");
-    inputColor.type = "color";
-    inputColor.click();
-
-    inputColor.addEventListener("input", (e)=> {
-        const color = e.target.value;
-        saveColorFijo(color);
-        rootElement.style.setProperty('--color',color);
-    });
-}
-
 function resetColorsDefault() {
     localStorage.removeItem("color-fijo");
     location.reload();
@@ -86,12 +77,21 @@ function resetColorsDefault() {
 const getColorFijo = ()=> localStorage.getItem("color-fijo");
 const saveColorFijo = (color)=> localStorage.setItem("color-fijo", color);
 
-document.addEventListener("dblclick", ()=> {
+document.addEventListener("dblclick", (e)=> {
     (isFullScreen)
         ? document.exitFullscreen()
         : document.body.requestFullscreen();
 
     isFullScreen = !isFullScreen;
 });
+
+
+inputColor.addEventListener("input", (e)=> {
+    stopChangeColors();
+    const color = e.target.value;
+    saveColorFijo(color);
+    rootElement.style.setProperty('--color',color);
+});
+resetDefaultColors.addEventListener("click", resetColorsDefault);
 
 mostrarDataDate();
